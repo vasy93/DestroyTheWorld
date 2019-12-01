3e44869283b437f5c5623e231711931ec50d3599
Badilo <vasy_93@yahoo.com>
2019-12-01 23:23:30 +0200

    hello world

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Badilo on 01/12/2019.
+//  Copyright © 2019 Badilo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Hello World!")
+    }
+
+
+}
+
